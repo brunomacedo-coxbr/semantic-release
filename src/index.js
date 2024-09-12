163f34a0e3974835507157d363dc54765d6a9aa8
Bruno-Santos <bruno.santos@coxautoinc.com>
2024-09-12 12:17:59 -0300

    feat(function): password random generate
    it generates a randon password for security issues
    
    #1230

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 /**
- * Documentation test
+ * Function to generate a random password
  */
-export function semanticReleaseGenerator() {
-  return "This is an example code, follow the steps on README.";
+export function generateRandomPassword(length = 12) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
 }
