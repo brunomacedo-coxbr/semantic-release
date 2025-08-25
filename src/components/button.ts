1a97788e387fc54d6e0d683577af583fd25e2bb7
brunomacedo-coxbr <bruno.macedo@coxautomotive.com.br>
2025-08-25 14:38:50 -0300

    fix(Release): create artifact

---
+++ src/components/button.ts
@@ -0,0 +1,12 @@
+/**
+ * Function to generate a random PASSWORD
+ */
+export function generateRandomPassword2(length: number = 12): string {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
+}
